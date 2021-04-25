0b50f9bc2021ef3362a46e992a692715085b033c
dkp1903 <dushyantpathak6789@gmail.com>
2021-04-25 07:14:24 +0000

    basher commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
-      
+         <Nav />
+        <Main />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Nav/styles.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core'
+
+export default makeStyles((theme) => ({
+    root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
